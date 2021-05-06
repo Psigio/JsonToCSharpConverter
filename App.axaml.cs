3cf61a26fa1684783baf1f1c87745aad1a38fc6a
Jon D <Psigio@users.noreply.github.com>
2021-05-06 18:33:38 +0100

    Initial commit using avalonia.mvvm target

---
+++ App.axaml.cs
@@ -0,0 +1,44 @@
+using System;
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using JsonToCSharpConverter.ViewModels;
+using JsonToCSharpConverter.Views;
+
+namespace JsonToCSharpConverter
+{
+    public class App : Application
+    {
+        private MainWindowViewModel _context;
+
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                _context = new MainWindowViewModel();
+                desktop.MainWindow = new MainWindow
+                {
+                    DataContext = _context,
+                };
+                desktop.Exit += HandleExit;
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+
+        private void HandleExit(object sender, ControlledApplicationLifetimeExitEventArgs e)
+        {
+            _context?.Dispose();
+            var source = sender as IClassicDesktopStyleApplicationLifetime;
+            if (source != null)
+            {
+                source.Exit -= HandleExit;
+            }
+        }
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using ReactiveUI;
+
+namespace JsonToCSharpConverter.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase, IDisposable
+    {
+        private readonly IDisposable _inputSubscription;
+        private string _inputValue = "{\"Paste\": \"JSON Here\"}";
+        private string _outputValue = "Result will appear here";
+        private bool disposedValue;
+
+        public MainWindowViewModel()
+        {
+            _inputSubscription
+                = this.WhenAnyValue(x => x.InputValue)
+                    .Subscribe(x => ParseAndConvert(x));
+        }
+
+        private void ParseAndConvert(string inputJson)
+        {
+            try
+            {
+                var jObject = JObject.Parse(inputJson);
+                OutputValue = Convert(jObject) + ";";
+            }
+            catch (Exception ex)
+            {
+                OutputValue = $"Not valid Json: {ex.Message}";
+            }
+        }
+
+        private string Convert(JToken input)
+        {
+            // Build string
+            var sb = new StringBuilder();
+            switch (input)
+            {
+                case JArray array:
+                    {
+                        sb.AppendLine("new [] {");
+                        var values = array
+                            .Where(x => x != null)
+                            .Select(v => Convert(v));
+                        sb.AppendJoin($",{Environment.NewLine}", values);
+                        sb.Append("}");
+                    }
+                    break;
+                case JObject jObject:
+                    {
+                        sb.AppendLine("new {");
+                        var values = jObject
+                            .Properties()
+                            .Where(x => x != null)
+                            .Select(v => Convert(v));
+                        sb.AppendJoin($",{Environment.NewLine}", values);
+                        sb.Append("}");
+                    }
+                    break;
+                case JProperty jProperty:
+                    {
+                        var preamble = jProperty.Parent?.Type != JTokenType.Array
+                            ? $"{jProperty.Name} = "
+                            : "";
+                        sb.Append(preamble);
+                        sb.Append(Convert(jProperty.Value));
+                        break;
+                    }
+                case JValue jValue:
+                    {
+                        var rawValue = jValue.Value?.ToString();
+                        switch (jValue.Type)
+                        {
+                            case JTokenType.Object:
+                            case JTokenType.Array:
+                                sb.Append(Convert(jValue));
+                                break;
+                            case JTokenType.Float:
+                            case JTokenType.Integer:
+                            case JTokenType.Boolean:
+                                sb.Append(rawValue);
+                                break;
+                            default:
+                                sb.Append($"\"{jValue.Value?.ToString()}\"");
+                                break;
+                        }
+                    }
+                    break;
+                default:
+                    throw new InvalidOperationException($"JContainer type {input.GetType().FullName} was not expected");
+            }
+            return sb.ToString();
+        }
+
+        public string InputValue
+        {
+            get => _inputValue;
+            set => this.RaiseAndSetIfChanged(ref _inputValue, value);
+        }
+
+        public string OutputValue
+        {
+            get => _outputValue;
+            set => this.RaiseAndSetIfChanged(ref _outputValue, value);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _inputSubscription.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
