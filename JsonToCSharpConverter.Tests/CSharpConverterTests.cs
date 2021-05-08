e819f19cfe9f47d60c680cf71eef0d3f366b3ebd
Jon D <Psigio@users.noreply.github.com>
2021-05-08 15:08:49 +0100

    Add round-trip tests to confirm generated code matches input.

--- JsonToCSharpConverter.Tests/CSharpConverterTests.cs
+++ JsonToCSharpConverter.Tests/CSharpConverterTests.cs
@@ -1,3 +1,7 @@
+using System.Text.Json.Serialization;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Xunit;
 
@@ -18,6 +22,28 @@ namespace JsonToCSharpConverter.Tests
             var candidate = CreateCandidate();
             var output = candidate.Convert(PrepareInput(input));
             Assert.Equal(expected, output);
+        }
+
+        [Theory]
+        [InlineData("{\"a\":\"b\"}")]
+        [InlineData("{\"a\":5}")]
+        [InlineData("{\"a\":true}")]
+        [InlineData("{\"a\":[\"b\", \"c\"]}")]
+        [InlineData("{\"a\":[5, 6]}")]
+        [InlineData("{\"a\":[true, false]}")]
+        [InlineData("{\"inner\": { \"a\":\"b\", \"b\": 5, \"c\": true, \"d\":[true, false]}}")]
+        public async void Roundtrip_Works(string input)
+        {
+            var candidate = CreateCandidate();
+            var converted = await candidate.ParseAndConvert(input);
+            // Use the Roslyn Scripting package to execute the output and return the generated output
+            var script = $"{converted} return a;";
+            var runtimeValue = await CSharpScript.EvaluateAsync(script, ScriptOptions.Default);
+            // Use Newtonsoft to convert back to JObject
+            var output = JsonConvert.SerializeObject(runtimeValue);
+            // Roundtrip the input string through Newtonsoft to ensure the formatting is the same
+            var formattedInput = JsonConvert.SerializeObject(PrepareInput(input));
+            Assert.Equal(formattedInput, output);
         }
 
         private JObject PrepareInput(string input) => JObject.Parse(input);
--- JsonToCSharpConverter/CSharpConverter.cs
+++ JsonToCSharpConverter/CSharpConverter.cs
@@ -90,6 +90,5 @@ namespace JsonToCSharpConverter
             }
             return sb.ToString();
         }
-
     }
 }
