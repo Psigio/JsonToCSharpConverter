0cde28b5195195832889b38867c2d81042643eb6
psigio <psigio>
2021-05-11 22:06:42 +0100

    Unit tests for IsCopyEnabled

--- JsonToCSharpConverter.Tests/MainWindowViewModelTests.cs
+++ JsonToCSharpConverter.Tests/MainWindowViewModelTests.cs
@@ -2,6 +2,7 @@ using Avalonia.Input.Platform;
 using JsonToCSharpConverter.Abstracts;
 using JsonToCSharpConverter.ViewModels;
 using Moq;
+using System;
 using Xunit;
 
 namespace JsonToCSharpConverter.Tests
@@ -66,6 +67,31 @@ namespace JsonToCSharpConverter.Tests
             candidate.Dispose();
             candidate.GenerateFullSnippet = false;
             _mockCSharpConvert.Verify(x => x.ParseAndConvert(It.IsAny<string>(), false, It.IsAny<string>()), Times.Never());
+        }
+
+        [Fact]
+        public void Valid_InputValue_Sets_CanCopy_To_True()
+        {
+            using (var candidate = CreateCandidate())
+            {
+                var input = "{\"a\":\"b\"}";
+                candidate.InputValue = input;
+                Assert.True(candidate.IsCopyEnabled);
+            }
+        }
+
+        [Fact]
+        public void Invalid_InputValue_Sets_CanCopy_To_False()
+        {
+            _mockCSharpConvert.Setup(x => x.ParseAndConvert(It.IsAny<string>(),
+            It.IsAny<bool>(), It.IsAny<string>()))
+                .Throws(new Exception("Test"));
+            using (var candidate = CreateCandidate())
+            {
+                var input = "{\"a\":}";
+                candidate.InputValue = input;
+                Assert.False(candidate.IsCopyEnabled);
+            }
         }
 
         private MainWindowViewModel CreateCandidate()
